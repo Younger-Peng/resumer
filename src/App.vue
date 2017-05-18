3e9aa644515719c390a875c50979bad39284b1b9
plearner <953413537@qq.com>
2017-05-18 22:33:44 +0800

    修改样式

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <div id="app">
-    <Topbar/>
-    <ResumeEditor/>
-    <ResumePreview/>
+  <div class="page">
+    <header>
+      <Topbar/>   
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
   </div>
 </template>
 
 <script>
+import 'normalize.css/normalize.css'
+import './assets/reset.css'
+
 import Topbar from './components/Topbar'
 import ResumeEditor from './components/ResumeEditor'
 import ResumePreview from './components/ResumePreview'
-
-import 'normalize.css/normalize.css'
-import './assets/reset.css'
 
 export default {
   name: 'app',
@@ -26,5 +30,27 @@ export default {
 </script>
 
 <style>
-
+  .page{
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+  .page>main{
+    flex: 1;
+  }
+  .page>main{
+    min-width: 1024px;
+    max-width: 1440px;
+    margin: 0;
+    display: flex;
+    justify-content: space-around;
+  }
+  #resumeEditor{
+    width: 35%;
+    background: #444;
+  }
+  #resumePreview{
+    width: 61.66667%;
+    background: #777;
+  }
 </style>
