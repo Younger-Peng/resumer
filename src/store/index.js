b2744847b3a3e13c7abbdbbad637a61115f5f1d8
plearner <953413537@qq.com>
2017-05-20 21:37:30 +0800

    使用 setter 让数据可写

--- src/store/index.js
+++ src/store/index.js
@@ -45,6 +45,9 @@ export default new Vuex.Store({
   mutations: {
     increment(state) {
       state.count ++
+    },
+    switchTab (state, payload){
+      state.selected = payload
     }
   }
 })
