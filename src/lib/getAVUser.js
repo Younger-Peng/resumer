3e427c3cbf78a21ce41917fa6c071b11ee5e56b7
plearner <953413537@qq.com>
2017-05-21 18:23:47 +0800

    修复一个 BUG:
    如果将user.id 和 user.username 改为undefined
    
    那么 Vue.js 将无法监听user的变化

--- src/lib/getAVUser.js
+++ src/lib/getAVUser.js
@@ -2,5 +2,8 @@ import AV from '../lib/leancloud'
 
 export default function(user){
   var {id, attributes:{username}} = user || AV.User.current() || {attributes: {}}
-  return {id:id,username:username}
+  return {
+    id: id || '',
+    username:username || ''
+  }
 }
