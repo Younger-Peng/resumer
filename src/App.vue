ebd4b1f3861fbc3145752421e497abd30c8e1315
plearner <953413537@qq.com>
2017-05-21 17:33:01 +0800

    页面载入时获取已登录用户

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,8 @@
   import ResumePreview from './components/ResumePreview'
   import icons from './assets/icons'
   import store from './store/index'
+  import AV from './lib/leancloud'
+  import getAVUser from './lib/getAVUser'
 
   export default {
     name: 'app',
@@ -32,6 +34,7 @@
         state = JSON.parse(state)
       }
       this.$store.commit('initState', state)
+      this.$store.commit('setUser', getAVUser())
     }
   }
 </script>
