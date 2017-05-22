cac7a0a1f4ea3f24c222cd4f6897e30e31b94127
plearner <953413537@qq.com>
2017-05-22 11:49:15 +0800

    删除所有的假数据

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 
 > A Vue.js project
 
-## [预览链接](https://younger-peng.github.io/resumer/dist/#/)
+#### step-1 双向数据流
 
+##### [预览链接](https://younger-peng.github.io/resumer/dist/#/)
+
+#### step-2 单向数据流
+
+##### [预览链接](https://younger-peng.github.io/resumer/step-2/dist/#/)
--- step-2/src/store/index.js
+++ step-2/src/store/index.js
@@ -20,42 +20,36 @@ export default new Vuex.Store({
         { field: 'contacts', icon: 'phone' },
       ],
       profile: {
-        name: '彭某某',
-        city: '城乡结合部',
-        title: '杀马特发型师',
-        birthday: '1989-11-26'
+        name: '',
+        city: '',
+        title: '',
+        birthday: ''
       },
       workHistory: [
         {
-          company: '国家施工队射鸡尸',
-          content: `公司总部设在美国洛杉矶，先后在北京、上海成立分公司，专注于搬砖码墙，主打刮墙，
-                    我的主要工作如下：
-                      1. 完成混凝土搅拌
-                      2. 码墙，勾缝`
+          company: '',
+          content: ``
         },
         {
-          company: '狗急跳墙责任有限公司',
-          content: `公司总部设在美国洛杉矶，先后在北京、上海成立分公司，专注于搬砖码墙，主打刮墙，
-                    我的主要工作如下：
-                      1. 完成混凝土搅拌
-                      2. 码墙，勾缝`
+          company: '',
+          content: ``
         }
       ],
       education: [
-        { school: '华北野鸡大学', content: '本科' },
-        { school: '沙发特高中', content: '一个打仨' }
+        { school: '', content: '' },
+        { school: '', content: '' }
       ],
       projects: [
-        { name: 'project A', content: '文字' },
-        { name: 'project B', content: '文字' }
+        { name: '', content: '' },
+        { name: '', content: '' }
       ],
       awards: [
-        { name: '再来十瓶', content: '连续十次获得 【再来一瓶】 奖励 ' },
-        { name: '英语口语大赛', content: '三等奖' }
+        { name: '', content: '' },
+        { name: '', content: '' }
       ],
       contacts: [
-        { contact: 'phone', content: '13812345678'},
-        { contact: 'qq', content: '123456789'}
+        { contact: '', content: ''},
+        { contact: '', content: ''}
       ]
     },
   },
