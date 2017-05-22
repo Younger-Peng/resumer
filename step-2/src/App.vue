a19bb5700eb5bb2d18334b799b614d105f90ec54
plearner <953413537@qq.com>
2017-05-22 18:07:00 +0800

    添加 /Preview 页面并整理样式

--- step-2/src/App.vue
+++ step-2/src/App.vue
@@ -10,6 +10,7 @@
   import AV from './lib/leancloud'
   import getAVUser from './lib/getAVUser'
   import icons from './assets/icons'
+  import './assets/ui.scss'
 
   document.body.insertAdjacentHTML('afterbegin', icons)
   export default {
@@ -38,77 +39,5 @@
 </script>
 
 <style lang="scss">
-  .page{
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    background: #EAEBEC;
-    >main{
-      flex: 1;
-    }
-    >main{
-      min-width: 1024px;
-      max-width: 1440px;
-      width: 100%;
-      margin-top: 16px;
-      margin-bottom: 16px;
-      display: flex;
-      justify-content: space-between;
-      padding: 0 16px;
-      align-self: center;
-      #resumeEditor{
-        min-width: 35%;
-      }
-      #resumePreview{
-        flex-grow: 1;
-        margin-left: 16px;
-        
-      }
-    }
-  }
-  svg.icon{
-    height: 1em;
-    width: 1em;
-    fill: currentColor;
-    vertical-align: -0.1em;
-    font-size: 16px;
-  }
-  .button{
-    width: 72px;
-    height: 32px;
-    border: none;
-    cursor: pointer;
-    font-size: 18px;
-    background: #ddd;
-    color: #222;
-    text-decoration: none;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    vertical-align: middle;
-    &:hover{
-      box-shadow: 1px 1px 1px hsla(0,0,0,0.5)
-    }
-    &.primary{
-      background: #02af5f;
-      color: white;
-    }
-    &.small {
-      height: 24px;
-      font-size: inherit;
-      width: auto;
-      padding-left: 1em;
-      padding-right: 1em;
-    }
-    .actions{
-      display: flex;
-      .userActions{
-        .welcome{
-          margin-right: 0.5em;
-        }
-      }
-    }
-  }
-  
   
 </style>
