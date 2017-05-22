65c13cfb51fd3baaba33f36e2401f1b427ec53e0
plearner <953413537@qq.com>
2017-05-22 11:31:33 +0800

    删除 保存 和 预览 按钮
    1. 实时保存，因此不需要用户点保存
    
    2. 预览按钮移到其他地方

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="topbar">
     <div class="wrapper">
-      <span class="logo">Resumer</span>
+      <span class="logo">Resumer1</span>
       <div class="actions">
         <div v-if="logined" class="userActions">
           <span class="welcome">你好，{{user.username}}</span>
@@ -11,8 +11,6 @@
           <a class="button primary" href="#" @click.prevent="signUpDialogVisible = true">注册</a>     
           <a class="button" href="#" @click.prevent="signInDialogVisible = true">登录</a>
         </div>
-        <button class="button primary">保存</button>
-        <button class="button">预览</button>
       </div>
     </div>
     <MyDialog title="注册" :visible="signUpDialogVisible" @close="signUpDialogVisible = false">
@@ -106,7 +104,6 @@ export default {
     .actions{
       display: flex;
       .userActions{
-        margin-right: 3em;
         .welcome{
           margin-right: 0.5em;
         }
