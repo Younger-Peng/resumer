6ae929819fbbe04492ad791b1a50ce8aa4230c83
plearner <953413537@qq.com>
2017-05-22 14:06:58 +0800

    使用vue-i18n翻译界面文本

--- step-2/src/main.js
+++ step-2/src/main.js
@@ -3,6 +3,60 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueI18n from 'vue-i18n'
+
+var locales = {
+  en: {
+    resume: {
+      name: 'Name',
+      city: 'City',
+      title: 'Job Title',
+      birthday: 'Birthday'
+    }
+  },
+  zh_CN: {
+    resume: {
+      profile: {
+        name: '名称',
+        city: '城市',
+        title: '',
+        birthday: ''
+      },
+      workHistory: {
+        _: '',
+        company: '',
+        details: ''
+      },
+      education: {
+        _: '',
+        school: '',
+        details: ''
+      },
+      projects: {
+        _: '',
+        name: '',
+        details: ''
+      },
+      awards: {
+        _: '',
+        name: '',
+        details: ''
+      },
+      contacts: {
+        _: '',
+        contact: '',
+        content: ''
+      }
+    }
+  }
+}
+
+Vue.ues(VueI18n)
+Vue.config.lang = 'zh_CN'
+
+Object.keys(locales).forEach(function(lang)){
+  Vue.locale(lang, locales[lang])
+}
 
 Vue.config.productionTip = false
 
